75b2e43f7e33a4b5c30175078e3b6bdcbebd031c
Adriane Rodrigues <dryrodrigues2009@gmail.com>
2020-01-30 18:18:18 -0300

    objetos e diferença de null e undefined

---
+++ fundamentos/variaveis_e_constantes/nullUndefined.js
@@ -0,0 +1,8 @@
+let valor 
+
+//a variável não foi inicializada, por isso o undefined
+console.log(valor)
+
+valor = null
+//o valor foi definido como nulo, ou seja, não aponta para nenhum local de memória
+console.log(valor)
---
+++ fundamentos/variaveis_e_constantes/object.js
@@ -0,0 +1,24 @@
+//criando um objeto
+const pessoa = {}
+
+//definindo chave:valor
+pessoa.nome = "Adriane"
+pessoa.idade = 22 
+
+console.log("----------------chave:valor----------------")
+console.log(pessoa)
+
+////////////////////////////Segundo método de criar um objeto////////////////////////////
+
+const guitarra = {
+    marca: "ibanez",
+    cor: "laranja"
+}
+
+console.log(guitarra)
+
+/* 
+    Observação: objeto não é a mesma coisa que json.
+    '{ "nome" : "adriane", "idade": 22}'
+*/
+
