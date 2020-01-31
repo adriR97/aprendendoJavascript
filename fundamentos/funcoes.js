2346ff6b4573346605bd8993a9bb2cc906e49d25
Adriane Rodrigues <dryrodrigues2009@gmail.com>
2020-01-31 16:57:04 -0300

    funcoes arrow

--- fundamentos/funcoes.js
+++ fundamentos/funcoes.js
@@ -9,3 +9,19 @@ function soma(a, b = 3){
     return a + b
 }
 console.log(soma(2))
+
+//armazenando uma função em uma variável
+const somando = function(a, b){
+    console.log(a + b)
+}
+somando(5, 7)
+
+//armazenando uma função arrow em uma variável
+const somaAB = (a, b) => {
+    return a + b
+}
+console.log(somaAB(10, 20))
+
+//retorno implícito de uma função arrow de forma mais simples
+const somaAB2 = (a, b) => a + b
+console.log(somaAB2(5, 4))
