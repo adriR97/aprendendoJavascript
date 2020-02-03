8280c4ccd40d5c9686e4b853b38ddf0331d774ce
Adriane Rodrigues <dryrodrigues2009@gmail.com>
2020-02-03 17:53:03 -0300

    operadores lógicos

---
+++ fundamentos/operadores/atribuicao.js
@@ -0,0 +1,14 @@
+//atribuindo valor às variáveis
+const a = 7
+let b = 3
+
+console.log("------------------Soma-----------------")
+console.log(b += a)                                                    //b = b + a
+console.log("\n---------------Subtração---------------")
+console.log(b -= 4)                                                    //b = b - 4
+console.log("\n-------------Multiplicação-------------")
+console.log(b *= 2)                                                    //b = b * 2
+console.log("\n----------------Divisão----------------")
+console.log(b /= 2)                                                    //b = b / 2
+console.log("\n-------------Módulo(resto)-------------")
+console.log(b %= 2)                                                    //b = b % 2
